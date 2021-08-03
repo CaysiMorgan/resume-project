a9bf26b9131613eaf819d882d33844282a475391
caysi morgan <Caysi.s.morgan@gmail.com>
2021-08-03 13:35:54 -0500

    Updated

--- Program.cs
+++ Program.cs
@@ -1,10 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Collections;
-using System.Collections.Specialized;
 namespace resume
 {
     class Program
@@ -12,98 +9,91 @@ namespace resume
         static void Main(string[] args)
         {
             //create new instance of a resume
-            var Newpeople = new resume();
+            var newpeople = new resume();
 
             Console.WriteLine(" Welcome to your Resume ");
             Console.WriteLine(" ***************************");
             Console.WriteLine("Please enter your name:");
             Console.Write(" First name:");
-            Newpeople.Fname = Console.ReadLine();
+            newpeople.firstName = Console.ReadLine();
             Console.Write(" Last name:");
-            Newpeople.Lname = Console.ReadLine();
+            newpeople.lastName = Console.ReadLine();
             Console.Write("Please enter your address:");
-            Newpeople.address = Console.ReadLine();
+            newpeople.address = Console.ReadLine(); 
             Console.Write("Please enter your telephone number:");
-            Newpeople.telenum = Console.ReadLine();
-            Console.Write("Please enter your Email Address:");
-            Newpeople.email = Console.ReadLine();
-            Console.WriteLine("Your summary paragraph:");
-            Newpeople.summary = Console.ReadLine();
-
-
-            string work = null;
-            var listOfWork = new List<string>();
-            Console.WriteLine("Please enter your list of work experience:");
-            for (int i = 0; i < 4; i++)
-            {
-                Console.Write("-");
-                work = Console.ReadLine();
-                listOfWork.Add(work);
-
-            }
-
-
-            string education = null;
-            var listOfedu = new List<string>();
-            Console.WriteLine("Please enter your educational qualifications:");
-            for (int i = 0; i < 4; i++)
-            {
-                Console.Write("-");
-                education = Console.ReadLine();
-                listOfedu.Add(education);
-
-            }
-            string skill = null;
-            var listOfSkills = new List<string>();
+            newpeople.telenumber = Console.ReadLine();
+            Console.Write("Please enter your email address:");
+            newpeople.email = Console.ReadLine();
+            Console.WriteLine("Your Summary paragraph:");
+            newpeople.Summary = Console.ReadLine();
             Console.WriteLine("Please enter your list of skills:");
-            for (int i = 0; i < 4; i++)
-            {
-                Console.Write("-");
-                skill = Console.ReadLine();
-                listOfSkills.Add(skill);
-
-            }
-            Console.WriteLine(Newpeople.GetDescription());
-
-            
-            Console.WriteLine("\n\nEducation: \n*********** ");
-            for (int i = (listOfedu.Count - 1); i >= 0; i--)
-            {
-                var item = listOfedu[i];
-                Console.WriteLine($"-{item}");
-            }
-            Console.WriteLine("\n\nWork Experience: \n*********** ");
-            for (int i = (listOfWork.Count - 1); i >= 0; i--)
-            {
-                var item = listOfWork[i];
-                Console.WriteLine($"-{item}");
-            }
-            Console.WriteLine("\n\nSkills: \n*********** ");
-            for (int i = (listOfSkills.Count - 1); i >= 0; i--)
-            {
-                var item = listOfSkills[i];
-                Console.WriteLine($"-{item}");
-            }
+            newpeople.skills.Add(Console.ReadLine());
+            Console.WriteLine("Please enter your list of educational qualifications:");
+            newpeople.education.Add(Console.ReadLine());
+            Console.WriteLine("Please enter your list of work experince :");
+            newpeople.WorkExperience.Add(Console.ReadLine());
+            Console.WriteLine(newpeople.GetDescription());
         }
         public class resume
         {
-            public string Fname { get; set; }
-            public string Lname { get; set; }
-            public string telenum { get; set; }
+            public resume()
+            {
+                skills = new List<string>();
+                WorkExperience = new List<string>();
+                education = new List<string>();
+            }
+
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string telenumber { get; set; }
             public string address { get; set; }
             public string email { get; set; }
-            public string summary { get; set; }
-            public string GetDescription()
-            {
-
-                return $"\n \n{Fname} {Lname}'s\nResume\n_____________________\nContact information: \n************** \nAddress: {address}\nTelephone: {telenum} \nEmail:{email} \nPersonal profile:\n {summary} .";
-
-                
-            }
+            public string Summary { get; set; }
+            public List<string>  skills { get; set; }
             
-        
+            public List<string> WorkExperience { get; set; }
+            public List<string> education { get; set; }
+            public string GetDescription() 
+            {
+                var stringBuilder = new StringBuilder();
+                
+                    stringBuilder.AppendLine($"{firstName} {lastName}");
+                    stringBuilder.AppendLine($"Resume");
+                    stringBuilder.AppendLine($"___________");
+                    stringBuilder.AppendLine($"Contact Information");
+                    stringBuilder.AppendLine($"*******************");
+                    stringBuilder.AppendLine($"address:{address}");
+                    stringBuilder.AppendLine($"Telephone: {telenumber}");
+                    stringBuilder.AppendLine($"E" +
+                        $"" +
+                        $"mail: {email}");
+                    stringBuilder.AppendLine($"Personal Profile:");
+                    stringBuilder.AppendLine($"{Summary}");
+                foreach (string Skill in 
+                    skills) 
+                {
+                    stringBuilder.AppendLine($"Skills:{Skill}");
+                }
+
+                foreach (string educational in
+                    education)
+                {
+                    stringBuilder.AppendLine($"educational qualifications:{educational}");
+                }
+
+                foreach (string work in
+                    WorkExperience)
+                {
+                    stringBuilder.AppendLine($"Work Experience:{work}");
+                }
+                return stringBuilder.ToString();
+                
+
+            }
+
+        } 
     }
         
 }
    
-}
+
